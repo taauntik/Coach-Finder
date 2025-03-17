c456052d8fd79c46731cb9ed256cf3dc597c85d5
Tasnim Ahmed Auntik <devauntik@gmail.com>
2025-03-18 02:03:24 +0600

    add CoachItem component and update CoachesList to use it for displaying coach details

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -11,13 +11,27 @@
             <li v-for="coach in filteredCoaches" :key="coach.id">
                 {{ coach.firstName }}
             </li>
+            <coach-item
+                v-for="coach in filteredCoaches"
+                :key="coach.id"
+                :first-name="coach.firstName"
+                :last-name="coach.lastName"
+                :areas="coach.areas"
+                :id="coach.id"
+                :rate="coach.hourlyRate">
+            </coach-item>
         </ul>
         <h3 v-else>No coaches found!</h3>
     </section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 export default {
+    components: {
+        CoachItem
+    },
     computed: {
         filteredCoaches() {
             return this.$store.getters['coaches/coaches'];
@@ -28,3 +42,16 @@ export default {
     },
 }
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
