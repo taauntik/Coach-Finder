fb0379e2dde35de74962c9191f49c8d9a2d3d397
Tasnim Ahmed Auntik <devauntik@gmail.com>
2025-03-19 22:11:11 +0600

    refactor CoachesList and CoachItem components to use new UI components; add BaseCard, BaseButton, and BaseBadge

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -3,25 +3,27 @@
         FILTER
     </section>
     <section>
-        <div class="controls">
-            <button>Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
-        </div>
-        <ul v-if="hasCoaches">
-            <li v-for="coach in filteredCoaches" :key="coach.id">
-                {{ coach.firstName }}
-            </li>
-            <coach-item
-                v-for="coach in filteredCoaches"
-                :key="coach.id"
-                :first-name="coach.firstName"
-                :last-name="coach.lastName"
-                :areas="coach.areas"
-                :id="coach.id"
-                :rate="coach.hourlyRate">
-            </coach-item>
-        </ul>
-        <h3 v-else>No coaches found!</h3>
+        <base-card>
+            <div class="controls">
+                <base-button :mode="'outline'">Refresh</base-button>
+                <base-button link to="/register">Register as Coach</base-button>
+            </div>
+            <ul v-if="hasCoaches">
+                <li v-for="coach in filteredCoaches" :key="coach.id">
+                    {{ coach.firstName }}
+                </li>
+                <coach-item
+                    v-for="coach in filteredCoaches"
+                    :key="coach.id"
+                    :first-name="coach.firstName"
+                    :last-name="coach.lastName"
+                    :areas="coach.areas"
+                    :id="coach.id"
+                    :rate="coach.hourlyRate">
+                </coach-item>
+            </ul>
+            <h3 v-else>No coaches found!</h3>
+        </base-card>
     </section>
 </template>
 
